7b8ce0584157462c14f89c95161a7c8e5fbc3125
Vitaly Turovsky <vital2580@icloud.com>
2025-02-13 20:20:07 +0300

    up channel

--- src/customChannels.ts
+++ src/customChannels.ts
@@ -1,42 +1,40 @@
 customEvents.on('mineflayerBotCreated', () => {
-  bot.on('login', () => {
-    const CHANNEL_NAME = 'minecraft:webmodels'
+  const CHANNEL_NAME = 'minecraft-web-client:blockmodels'
 
-    const packetStructure = [
-      'container',
-      [
-        {
-          name: 'worldName',
-          type: ['pstring', { countType: 'i16' }]
-        },
-        {
-          name: 'x',
-          type: 'i32'
-        },
-        {
-          name: 'y',
-          type: 'i32'
-        },
-        {
-          name: 'z',
-          type: 'i32'
-        },
-        {
-          name: 'model',
-          type: ['pstring', { countType: 'i16' }]
-        }
-      ]
+  const packetStructure = [
+    'container',
+    [
+      {
+        name: 'worldName',
+        type: ['pstring', { countType: 'i16' }]
+      },
+      {
+        name: 'x',
+        type: 'i32'
+      },
+      {
+        name: 'y',
+        type: 'i32'
+      },
+      {
+        name: 'z',
+        type: 'i32'
+      },
+      {
+        name: 'model',
+        type: ['pstring', { countType: 'i16' }]
+      }
     ]
+  ]
 
-    bot._client.registerChannel(CHANNEL_NAME, packetStructure, true)
+  bot._client.registerChannel(CHANNEL_NAME, packetStructure, true)
 
-    bot._client.on(CHANNEL_NAME as any, (data) => {
+  bot._client.on(CHANNEL_NAME as any, (data) => {
 
-      console.log('received raw data:', data)
-      const { worldName, x, y, z, model } = data
-      console.log('Received model data:', { worldName, x, y, z, model })
-    })
-
-    console.log('registered channel')
+    console.log('received raw data:', data)
+    const { worldName, x, y, z, model } = data
+    console.log('Received model data:', { worldName, x, y, z, model })
   })
+
+  console.log(`registered ${CHANNEL_NAME} channel`)
 })
